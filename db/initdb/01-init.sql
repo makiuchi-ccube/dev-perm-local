b4ada7f098071831e0822e4d358e4345f0fc1ced
Takeshi FURUTA <furuta@mq-sol.jp>
2025-07-24 16:15:18 +0900

    db 修正

--- db/initdb/01-init.sql
+++ db/initdb/01-init.sql
@@ -20,8 +20,21 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 -- テーブルスペースの作成
 CREATE TABLESPACE devps_tbs
     OWNER devps
-    LOCATION '/var/lib/pgsql/14/data/devps_tbs';
-    
+    LOCATION '/var/lib/postgresql/data/devps_tbs';
+
 COMMENT ON TABLESPACE devps_tbs IS '稼働環境用テーブルスペース';
 
 GRANT CREATE ON TABLESPACE devps_tbs TO devps;
+
+
+CREATE DATABASE devps_db WITH
+OWNER = devps
+TEMPLATE template0
+ENCODING = 'UTF8'
+LC_COLLATE = 'ja_JP.UTF-8'
+LC_CTYPE = 'ja_JP.UTF-8'
+TABLESPACE = devps_tbs
+ALLOW_CONNECTIONS = true
+CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE devps_db IS '稼働環境用データベース';
