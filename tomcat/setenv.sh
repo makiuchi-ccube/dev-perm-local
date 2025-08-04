039aa4a40d455b56e9e7c35daaa85ae3cc052add
Takeshi FURUTA <furuta@mq-sol.jp>
2025-08-04 17:31:11 +0900

    tomcat geosever対応

--- tomcat/setenv.sh
+++ tomcat/setenv.sh
@@ -3,3 +3,6 @@ export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
 export CATALINA_OPTS="-server -Xmx1536m -Xms512m -XX:MaxMetaspaceSize=1536m -DALLOW_ENV_PARAMETRIZATION=true -Duser.timezone=Asia/Tokyo"
 export CATALINA_HOME=/usr/local/tomcat
 export CATALINA_BASE=/usr/local/tomcat
+export GEOSERVER_DATA_DIR=/opt/data
+export GEOSERVER_LOG_LOCATION=/var/log/geoserver.log
+
