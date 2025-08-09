450fb2408325d25050261528204202edb358ac9e
Hotaka Makiuchi <s.hotaka.makiuchi@iridge.jp>
2025-08-09 12:22:34 +0900

    dumpは不要。必要なデータはinitdbに配置

--- db/dump/gisdata/f_district_alter.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE f_district
-ADD result_column1 VARCHAR(256),
-ADD result_column2 VARCHAR(256),
-ADD result_column3 VARCHAR(256),
-ADD result_column4 VARCHAR(256),
-ADD result_column5 VARCHAR(256);
-
-UPDATE f_district SET result_column1=district_name;
